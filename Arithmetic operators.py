765933a506729384644666875ff0fbbecb18795b
Keerthana04-MH <keerthana2004keerthumanjulla@gmail.com>
2026-09-16 18:50:50 +0530

    Add arithmetic operations in Arithmetic operators.py
    Implement basic arithmetic operations with user input.

---
+++ Arithmetic operators.py
@@ -0,0 +1,21 @@
+a=int(input("Enter the number"))
+b=int(input("Enter the number"))
+print("addition=",a+b)
+print("Subraction=",a-b)
+print("Multiplication=",a*b)
+print("Division=",a/b)
+print("Floor Division =", a // b)
+print("Modulus =", a % b)
+print("Power =", a ** b)
+
+sample output
+Enter first number: 10
+Enter second number: 3
+
+Addition = 13
+Subtraction = 7
+Multiplication = 30
+Division = 3.3333333333333335
+print("Floor Division =", a // b)
+print("Modulus =", a % b)
+print("Power =", a ** b)
