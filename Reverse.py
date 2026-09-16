4eb4184c31ac0fed549c70404830cf5a721a0ee2
Keerthana04-MH <keerthana2004keerthumanjulla@gmail.com>
2026-09-16 19:03:39 +0530

    Add number reversal functionality in Reverse.py
    This script takes an integer input from the user and outputs its reverse.

---
+++ Reverse.py
@@ -0,0 +1,13 @@
+n = int(input("Enter a number: "))
+reverse = 0
+
+while n > 0:
+    digit = n % 10
+    reverse = reverse * 10 + digit
+    n = n // 10
+
+print("Reverse =", reverse)
+
+Sample output
+Enter a number: 12345
+Reverse = 54321
